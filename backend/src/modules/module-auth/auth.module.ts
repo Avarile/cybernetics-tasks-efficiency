6733a96f6dc33b98b3941bed3bfe0bc17cdebadc
Avarile <avarile@gmail.com>
2026-06-28 15:46:12 +1000

    feat(auth): wire PassportModule + JwtModule + auth providers

--- backend/src/modules/module-auth/auth.module.ts
+++ backend/src/modules/module-auth/auth.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import env from 'src/utils/env';
 import { PersonAccountRepository } from './account.repo';
+import { AuthSessionRepository } from './auth-session.repo';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { PasswordService } from './password.service';
+import { TokenService } from './token.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: env.JWT_ACCESS_TTL },
+    }),
+  ],
   controllers: [AuthenticationController],
-  providers: [PersonAccountRepository, AuthenticationService],
+  providers: [
+    PersonAccountRepository,
+    AuthSessionRepository,
+    AuthenticationService,
+    PasswordService,
+    TokenService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  exports: [PasswordService],
 })
 export class AuthModule {}
